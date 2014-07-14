b22305279a40fe0c165229fbabced0bb29d4a7ca
Mark Stephens <mr.morphic@gmail.com>
2014-07-14 21:51:10 +1200

    test for cancellationof one-off before it starts

--- gochronos_test.go
+++ gochronos_test.go
@@ -41,3 +41,74 @@ func TestAdd(t *testing.T) {
 
 	ClearAll()
 }
+
+func TestCancel(t *testing.T) {
+	count := 0
+
+	// Add a new one-off action. The action will count the number of times executed, and will set
+	// properties based on parameters.
+	sa := Add(NewOneOff(time.Now().Add(time.Second)),
+		func(args ...interface{}) {
+			count++
+		})
+
+	Remove(sa)
+
+	// kill all scheduled actions
+	time.Sleep(time.Second * 3)
+
+	if count != 0 {
+		t.Errorf("Expected one-off action to be cancelled and not executed, was executed %d times", count)
+	}
+
+	if len(schedule) > 0 {
+		t.Errorf("Expected schedule to empty, contains %d item(s)", len(schedule))
+	}
+
+	ClearAll()
+}
+
+func TestAddRecurring(t *testing.T) {
+	count := 0
+
+	// starting now, every second
+	ts := NewRecurring(map[string]interface{}{
+		"starttime": time.Now(),
+		"frequency": FREQ_SECOND,
+	})
+
+	Add(ts, func(args ...interface{}) {
+		count++
+	})
+
+	time.Sleep(time.Second * 10)
+
+	if count != 10 {
+		t.Errorf("Expected 1-sec recurring action running for 10 seconds to execute 10 times, was executed %d times", count)
+	}
+
+	ClearAll()
+}
+
+func TestAddRecurringInterval(t *testing.T) {
+	count := 0
+
+	// starting now, every 2 seconds.
+	ts := NewRecurring(map[string]interface{}{
+		"starttime": time.Now(),
+		"frequency": FREQ_SECOND,
+		"interval":  2,
+	})
+
+	Add(ts, func(args ...interface{}) {
+		count++
+	})
+
+	time.Sleep(time.Second * 10)
+
+	if count != 5 {
+		t.Errorf("Expected 2-sec recurring action running for 10 seconds to execute 5 times, was executed %d times", count)
+	}
+
+	ClearAll()
+}
